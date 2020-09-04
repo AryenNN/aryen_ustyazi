18bc446d014dd851d634acce6b6eeae75787f2bc
Ömer <70106602+AryenNN@users.noreply.github.com>
2020-09-04 16:33:20 +0300

    Create jns_client.lua

---
+++ jns_client.lua
@@ -0,0 +1,6 @@
+-- AryenN tarafından geliştirilmiştir 
+-- Developed by AryenN
+CurrentWeather = 'EXTRASUNNY'
+local lastWeather = CurrentWeather
+
+-- Dizeler / Programları senkronize edin / Zamanlayıcıyı senkronize edin.
